4660cc3bdfa2e2d64c6ab58ade4700124fe04ecd
HyeongMokJeong <jhm1062@naver.com>
2022-04-04 02:49:36 +0900

    Update HowManyWords.java

--- cbnu/src/HowManyWords.java
+++ cbnu/src/HowManyWords.java
@@ -1,4 +1,11 @@
 package cbnu;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Map;
 
 public class HowManyWords {
 	
@@ -7,7 +14,43 @@ public class HowManyWords {
 		return text;
 	}
 	
+	public static HashMap<Character, Integer> countChar(String text){
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		for (int i=0; i <= text.length() - 1; i++) {
+			if (map.containsKey(text.charAt(i))) { // 이미 Key값이 존재한다면
+				map.put(text.charAt(i), map.get(text.charAt(i)) + 1);
+			}
+			else 
+				map.put(text.charAt(i), 1);
+		}
+		
+//		List<Entry<Character, Integer>> map_list = new ArrayList<Entry<Character, Integer>>(map.entrySet());
+//		Collections.sort(map_list, new Comparator<Entry<String, Integer>>() {
+//			// compare로 값을 비교
+//			public int compare(Entry<String, Integer> obj1, Entry<String, Integer> obj2)
+//			{
+//				// 내림 차순으로 정렬
+//				return obj2.getValue().compareTo(obj1.getValue());
+//			}
+//		});
+		
+		return map;
+	}
+	
 	public static void main(String[] args) {
+		
+		
+		System.out.println("원문");
+		System.out.println(text());
+		String text = text().replace(" ", "").replace("\n", "").replace(".", "").replace(",", "");
+		HashMap<Character, Integer> map = countChar(text);
+		
+		System.out.println("-------------\n글자\t빈도수\n-------------");
+		for (Entry<Character, Integer> i: map.entrySet()) {
+			System.out.println(i.getKey() + "\t" + i.getValue());
+		}
+		
 		
 	}
 
